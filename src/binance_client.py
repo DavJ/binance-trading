55da6ae1c5ee226829f8294131939e3e16de72dd
David Jaros <jdavid.cz@gmail.com>
2021-05-06 22:18:45 +0200

    progress

--- src/binance_client.py
+++ src/binance_client.py
@@ -4,4 +4,9 @@ from config_private import BINANCE_API_KEY, BINANCE_API_SECRET
 client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
 products = client.get_exchange_info()
 
+symbols = [product['symbol'] for product in client.get_exchange_info()['symbols']]
+
+ticker = client.get_ticker(symbol=symbols[0])
+
+all_tickers = client.get_all_tickers()
 pass
---
+++ src/hello_world.py
@@ -0,0 +1,57 @@
+from decimal import Decimal
+from config/config_private import API_KEY, SECRET_KEY
+from binance.client import Client
+
+client = Client(API_KEY, SECRET_KEY)
+
+
+def max_converts(from_symbol, amount):
+    rates = client.get_all_tickers()
+    conversions = [{'to_symbol': rate['symbol'].split(from_symbol)[1],
+                    'value': Decimal(rate['price']) * Decimal(amount)}
+                   for rate in rates if rate['symbol'].split(from_symbol)[0] == '']
+    return sorted(conversions, key=lambda x: x['value'], reverse=True)
+
+
+def convert(from_symbol, to_symbol, amount):
+    rates = client.get_all_tickers()
+    try:
+       return {'to_symbol': to_symbol, 'value': [Decimal(rate['price']) * Decimal(amount)
+                                              for rate in rates if rate['symbol'] == from_symbol + to_symbol][0]
+              }
+    except:
+        return None
+
+def print_list_dict(list_dict):
+    _ = [print(item) for item in list_dict]
+
+
+#print(max_converts('BNB', '2.25'))
+
+
+#print_list_dict(client.get_all_tickers())
+
+
+print_list_dict(sorted(max_converts('BNB', '2.25'), key=lambda x: x['to_symbol']))
+
+print('>>>>''')
+print(convert('BNB', 'BUSD', '2.25'))
+print(convert('BUSD', 'BNB', '1'))
+
+print('>>>>''')
+
+print_list_dict(sorted(max_converts('AUD', 1618), key=lambda x: x['to_symbol']))
+
+print('>>>>''')
+
+print_list_dict(sorted(max_converts('BUSD', 1253), key=lambda x: x['to_symbol']))
+
+print('>>>>''')
+
+print('>>>>''')
+print(convert('BUSD', 'ETH', '1253'))
+
+print('>>>>')
+print_list_dict(sorted(max_converts('BEUR', 30), key=lambda x: x['to_symbol']))
+
+
