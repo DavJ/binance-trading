dc058a618d74d33a9da0f58e621839198b3a0e99
David Jaros <jdavid.cz@gmail.com>
2021-05-14 00:01:42 +0200

    kivi

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 mac: 
-	python3 -m venv p38 --system-site-packages
+	python3 -m venv p39 --system-site-packages
 	pip3 install -U pip
 	pip3 install --upgrade -r requirements.txt
 
@@ -7,6 +7,6 @@ prepare-ubuntu:
 	sudo apt-get install python3-venv
 
 ubuntu:
-	virtualenv --python=/usr/bin/python3.8 env	
+	virtualenv --python=/usr/bin/python3.9 env	
 	. ./env/bin/activate
 	sudo pip3 install --upgrade -r requirements.txt
--- src/playground/binance_client.py
+++ src/playground/binance_client.py
@@ -1,28 +1,11 @@
-from binance.client import Client
-from src.bot.config.config_private import BINANCE_API_KEY, BINANCE_API_SECRET
+from src.bot.tools.basic_tools import  get_binance_client
+client = get_binance_client()
 
-client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
 products = client.get_exchange_info()
-
 symbols = [product['symbol'] for product in client.get_exchange_info()['symbols']]
 
 ticker = client.get_ticker(symbol=symbols[0])
 
 all_tickers = client.get_all_tickers()
 
-
-
-
-pass
-
-
 trades = client.get_my_trades(symbol=symbols[0])
-
-
-
-
-
-
-
-
-pass
