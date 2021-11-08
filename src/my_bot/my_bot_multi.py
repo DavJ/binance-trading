2d73e0b9dd20d7615ecc6d0dfd4ea19a56cdd8e1
David Jaros <jdavid.cz@gmail.com>
2021-11-09 00:14:34 +0100

    fix

--- src/my_bot/my_bot_multi.py
+++ src/my_bot/my_bot_multi.py
@@ -84,7 +84,7 @@ class Application:
     def trade(self):
 
         def update_assets(assets):
-            for asset in assets():
+            for asset in assets:
                 asset.get_balance()
                 asset.update_last_trades()
 
