5fcc5fbd1d522a4d8f8bdf1058e10c008a9414d0
David Jaros <davidjaros@Davids-Mac-mini.local>
2022-02-12 01:32:37 +0100

    fix

--- src/my_bot/model/kalman2.py
+++ src/my_bot/model/kalman2.py
@@ -48,7 +48,7 @@ class Kalman2:
        :param dt:
        :return:
        """
-       return self.results_smoothed
+       return self.results_smoothed[0]
 
     @property
     def sorted_predictions(self):
