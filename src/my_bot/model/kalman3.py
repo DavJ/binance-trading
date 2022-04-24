69ceb01a89fc1b619e302dcdfe07121193f2a727
David Jaros <jdavid.cz@gmail.com>
2022-04-24 21:30:51 +0200

    add charts

--- src/my_bot/model/kalman3.py
+++ src/my_bot/model/kalman3.py
@@ -12,11 +12,15 @@ from src.my_bot.basic_tools import (Client, CONFIGURATION, get_binance_client, g
                                     use_async_client, get_async_web_socket_manager, get_threaded_web_socket_manager,
                                     get_trading_pairs, get_normalized_close_prices_async ,get_logger)
 
+from matplotlib import pyplot as plt
+from scipy import optimize
+#from scipy.interpolate import spline
+
 import logging
 
 logger = get_logger('Kalman2')
 
-
+#see also https://github.com/pykalman/pykalman
 class Kalman3:
 
     def __init__(self, trading_pairs=get_trading_pairs(), number_of_derivations=2):
@@ -41,15 +45,16 @@ class Kalman3:
                                                                                        interval=INTERVAL)))
 
         self.measurements_plus_derivations = np.copy(self.measurements)
-        derivative = np.diff(self.measurements)
+        derivative = np.diff(self.measurements, axis=0, prepend=0)
         for d in range(self.number_of_derivations):
-            self.measurements_plus_derivations = np.concatenate(self.measurements_plus_derivations, derivative)
+            self.measurements_plus_derivations = np.concatenate((self.measurements_plus_derivations, derivative), axis=1)
             if d < self.number_of_derivations - 1:
-                derivative = np.diff(derivative)
+                derivative = np.diff(derivative, axis=0, prepend=0)
 
         logger.info('smoothing data ...')
-        self.results_smoothed = self.kf.em(self.measurements_plus_derivations, n_iter=5).smooth(self.measurements_plus_derivations)[0]
+        self.results_smoothed = self.kf.em(self.measurements_plus_derivations, n_iter=10).smooth(self.measurements_plus_derivations)[0]
         logger.info('smoothing finished...')
+        self.plot()
 
     def predict_values(self):
        """
@@ -57,7 +62,7 @@ class Kalman3:
        :param dt:
        :return:
        """
-       return self.results_smoothed[0]
+       return self.results_smoothed[-1]
 
     @property
     def sorted_predictions(self):
@@ -84,3 +89,38 @@ class Kalman3:
        print('sorted predictions')
        for prediction in self.sorted_predictions:
            print(prediction)
+
+    def plot(self):
+        DISPLAYED_DATA = 450
+        POLYNOME_ORDER = 9
+        plt.title('Kalman filtering and smoothing')
+        plt.xlabel('sequence number [15min intervals]')
+        plt.xlabel('relative price')
+        x = np.arange(0, np.shape(self.measurements)[0])[-DISPLAYED_DATA:]
+        xfit = np.arange(0, POLYNOME_ORDER)
+        y1 = np.transpose(self.measurements_plus_derivations)[0][-DISPLAYED_DATA:]  #original data
+        y2 = np.transpose(self.results_smoothed)[0][-DISPLAYED_DATA:]  #smoothed
+        y3 = np.transpose(self.results_smoothed)[0 + self.number_of_pairs][-DISPLAYED_DATA:] # smoothed derivation
+        y4 = np.transpose(self.results_smoothed)[0 + 2 * self.number_of_pairs][-DISPLAYED_DATA:]  # smoothed second derivation
+
+        fig, axs = plt.subplots(2, 2)
+        fig.suptitle('Kalman filtering')
+
+        axs[0, 0].plot(x, y1, 'tab:blue')
+        axs[0, 0].plot(xfit, np.polyfit(x, y1, POLYNOME_ORDER), 'tab:cyan')
+        axs[0, 0].set_title('original')
+
+        axs[1, 0].plot(x, y2, 'tab:green')
+        axs[1, 0].plot(xfit, np.polyfit(x, y2, POLYNOME_ORDER), 'tab:cyan')
+        axs[1, 0].set_title('filtered')
+
+        axs[0, 1].plot(x, y3, 'tab:orange')
+        axs[0, 1].plot(xfit, np.polyfit(x, y3, POLYNOME_ORDER), 'tab:cyan')
+        axs[0, 1].set_title('filtered derivation')
+
+        axs[1, 1].plot(x, y4, 'tab:red')
+        axs[1, 1].plot(xfit, np.polyfit(x, y4, POLYNOME_ORDER), 'tab:cyan')
+        axs[1, 1].set_title('filtered second derivation')
+
+        plt.show()
+        pass
